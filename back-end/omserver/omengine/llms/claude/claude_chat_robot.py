1e13f85f6433f21801ad4cf8dfec4e7a1f34beb0
jackyhwei <j@rg4.net>
2024-10-16 09:23:33 +0800

    test

--- back-end/omserver/omengine/llms/claude/claude_chat_robot.py
+++ back-end/omserver/omengine/llms/claude/claude_chat_robot.py
@@ -1,8 +1,8 @@
-from claude_api import Client
-cookie = ""
-claude_api = Client(cookie, True)
+# from claude_api import Client
+# cookie = ""
+# claude_api = Client(cookie, True)
 
-prompt = "Hello, Claude!"
-conversation_id = claude_api.create_new_chat()['uuid']
-response = claude_api.send_message(prompt, conversation_id)
-print(response)
+# prompt = "Hello, Claude!"
+# conversation_id = claude_api.create_new_chat()['uuid']
+# response = claude_api.send_message(prompt, conversation_id)
+# print(response)
